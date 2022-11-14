bae8366f86ef8496bdb69751ec831fb9b1ba3fea
Benedikt Werner <1benediktwerner@gmail.com>
2022-11-15 00:05:41 +0100

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BugFixes
+{
+    [BepInPlugin("de.benediktwerner.stacklands.bugfixes", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        static ManualLogSource L;
+
+        private void Awake()
+        {
+            L = Logger;
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Combatable), nameof(Combatable.PerformSpecialHit))]
+        public static void FixHealLowestPrefix(SpecialHit specialHit, out SpecialHitTarget __state)
+        {
+            __state = specialHit.Target;
+            if (specialHit.HitType == SpecialHitType.HealLowest && specialHit.Target == SpecialHitTarget.Target)
+            {
+                specialHit.Target = SpecialHitTarget.RandomFriendly;
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Combatable), nameof(Combatable.PerformSpecialHit))]
+        public static void FixHealLowestPostfix(SpecialHit specialHit, SpecialHitTarget __state)
+        {
+            specialHit.Target = __state;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Cutscenes), nameof(Cutscenes.IslandIntroPack))]
+        public static void NoSecondIslandPackFromDemon(out bool __runOriginal, out IEnumerator __result)
+        {
+            __result = EndCutscenes();
+            __runOriginal = !WorldManager.instance.CurrentSaveGame.GotIslandIntroPack;
+        }
+
+        public static IEnumerator EndCutscenes()
+        {
+            Cutscenes.Stop();
+            yield break;
+        }
+
+        public void Update()
+        {
+            if (Time.realtimeSinceStartup > 30f)
+                return;
+            if (
+                Screen.currentResolution.width != OptionsScreen.CurrentWidth
+                || Screen.currentResolution.height != OptionsScreen.CurrentHeight
+            ) {
+                L.LogWarning("Found real solution != options solution. Adjusting.");
+                OptionsScreen.SetResolution();
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,21 @@
+# Stacklands BugFixes Mod
+
+Fixes various bugs in the game:
+- HealLowest special attacks no longer damage the healed units
+- Only spawn island ideas pack after the first Demon kill
+- Fix resolution setting sometimes not loading correctly
+
+## Manual Installation
+This mod requires BepInEx to work. BepInEx is a modding framework which allows multiple mods to be loaded.
+
+1. Download and install BepInEx from the [Thunderstore](https://stacklands.thunderstore.io/package/BepInEx/BepInExPack_Stacklands/).
+4. Download this mod and extract it into `BepInEx/plugins/`
+5. Launch the game
+
+## Links
+- Github: https://github.com/benediktwerner/Stacklands-BugFixes-Mod
+- Thunderstore: https://stacklands.thunderstore.io/package/benediktwerner/BugFixes
+
+## Changelog
+
+- v1.0: Initial release
