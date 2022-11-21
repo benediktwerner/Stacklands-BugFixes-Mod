52971d229d9c42d4d6dbc8dafb138336fdc1b0a6
Benedikt Werner <1benediktwerner@gmail.com>
2022-11-21 13:02:29 +0100

    Remove "heal lowest" fix (now in the game)

--- Plugin.cs
+++ Plugin.cs
@@ -20,24 +20,6 @@ namespace BugFixes
         }
 
         [HarmonyPrefix]
-        [HarmonyPatch(typeof(Combatable), nameof(Combatable.PerformSpecialHit))]
-        public static void FixHealLowestPrefix(SpecialHit specialHit, out SpecialHitTarget __state)
-        {
-            __state = specialHit.Target;
-            if (specialHit.HitType == SpecialHitType.HealLowest && specialHit.Target == SpecialHitTarget.Target)
-            {
-                specialHit.Target = SpecialHitTarget.RandomFriendly;
-            }
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(Combatable), nameof(Combatable.PerformSpecialHit))]
-        public static void FixHealLowestPostfix(SpecialHit specialHit, SpecialHitTarget __state)
-        {
-            specialHit.Target = __state;
-        }
-
-        [HarmonyPrefix]
         [HarmonyPatch(typeof(Cutscenes), nameof(Cutscenes.IslandIntroPack))]
         public static void NoSecondIslandPackFromDemon(out bool __runOriginal, out IEnumerator __result)
         {
--- Readme.md
+++ Readme.md
@@ -1,7 +1,6 @@
 # Stacklands BugFixes Mod
 
 Fixes various bugs in the game:
-- HealLowest special attacks no longer damage the healed units
 - When dragging a stack onto a fight, add all cards to it
 - Only spawn island ideas pack after the first Demon kill
 - Fix resolution setting sometimes not loading correctly
